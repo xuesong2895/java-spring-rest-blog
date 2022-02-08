d5d5699b3fc471038a53e42ede8922455b08749f
xuesong2895 <susan.shang@gmail.com>
2022-02-07 21:33:54 -0500

    Update PostRepository.java

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,29 +1,10 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
-
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
-}
+@RestResource
+public interface PostRepository extends JpaRepository<Post, Long> {}
